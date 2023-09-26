c014414a8b530cd33f451a4ea0a950f142274569
ericwarren9 <erwarren@ncsu.edu>
2023-09-26 11:15:35 -0400

    R file for Rendering Rmd file

---
+++ Warren_Project 2 Render File.R
@@ -0,0 +1,12 @@
+# Purpose: Make a File to Store produce a md file that produces our output from the corresponding Rmd file.
+
+rmarkdown::render("~/ST-558-Project-2/Warren_ST 558 Project 2.Rmd", 
+                  output_file = "README.md",
+                  output_format = "github_document", 
+                  output_options = list(
+                    toc = TRUE, 
+                    toc_depth = 3,
+                    number_sections = TRUE,
+                    df_print = "tibble"
+                  )
+)
